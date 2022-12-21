d4aa7c910600ff03e0f15a03e3745c802570a328
Кудряшов Максим Николаевич <mn.kudriashov@northis.ru>
2022-12-21 09:20:57 +0300

    [test]: Реализован метод генерации перестановок.

--- LinqQueryTest/Program.cs
+++ LinqQueryTest/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqQueryTest
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		#region Private
+		private static IEnumerable<string> GetAllSequences(IEnumerable<char> chars)
 		{
-			Console.WriteLine("Hello World!");
+			chars = chars.ToList();
+			if (chars.Count() == 1)
+			{
+				return chars.Select(p => p.ToString());
+			}
+
+			var output = new List<string>();
+			for (var i = 0; i < chars.Count(); i++)
+			{
+				foreach (var sequence in GetAllSequences(chars.Take(i)
+															  .Concat(chars.Skip(i + 1)
+																		   .Take(chars.Count() - i - 1)))
+							 .Where(sequence => !output.Contains(chars.ElementAt(i) + sequence)))
+				{
+					output.Add(chars.ElementAt(i) + sequence);
+				}
+			}
+
+			return output;
 		}
+
+		private static void Main(string[] args)
+		{
+			var chars = new[]
+			{
+				'A',
+				'B',
+				'C'
+			};
+
+			var result = GetAllSequences(chars);
+
+			foreach (var str in result)
+			{
+				Console.WriteLine(str);
+			}
+		}
+		#endregion
 	}
 }
